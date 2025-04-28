2e754b12bbac10820ee093bdad77a22262650eef
André <fonsecafontesandre@gmail.com>
2025-04-28 15:02:24 -0300

    more about the store

--- Testes/seconde_test/script/homePage.js
+++ Testes/seconde_test/script/homePage.js
@@ -4,3 +4,6 @@ let homeButton = document.querySelector('#testMenu');
 function home(){
     homeButton.classList.toggle('testMenu1');
 }
+function removeClass(){
+    homeButton.classList.remove('testMenu1');
+}
