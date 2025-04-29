4d056a37ba3dfd8b0849b3c0070395a17b6cd473
André <fonsecafontesandre@gmail.com>
2025-04-29 15:09:54 -0300

    more about the store

--- Testes/seconde_test/script/homePage.js
+++ Testes/seconde_test/script/homePage.js
@@ -7,3 +7,32 @@ function home(){
 function removeClass(){
     homeButton.classList.remove('testMenu1');
 }
+////////////////////////////////////////////////////////
+let allProducts = document.querySelectorAll('.toCart');
+
+allProducts.forEach((product) => {
+    let btnPlus = product.querySelector('.btnPlus');
+    let btnMinus = product.querySelector('.btnMinus');
+    let quantDisplay = product.querySelector('.productQuant');
+    let count = 0;
+
+    btnPlus.addEventListener('click', () => {
+        count++;
+        quantDisplay.innerHTML = count;
+    });
+
+    btnMinus.addEventListener('click', () => {
+        if (count > 0) {
+            count--;
+            quantDisplay.innerHTML = count;
+        }
+    });
+});
+
+////////////////////////////////////////////////////////
+let btnAddToCart = document.querySelector('.addToCart');
+
+btnAddToCart.addEventListener('click', () => {
+
+})
+////////////////////////////////////////////////////////
