36a93703c03ffaf770e0be50458ece22eb137b01
André <fonsecafontesandre@gmail.com>
2025-05-03 10:27:05 -0300

    almost finished

--- Testes/seconde_test/script/homePage.js
+++ Testes/seconde_test/script/homePage.js
@@ -11,28 +11,83 @@ function removeClass(){
 let allProducts = document.querySelectorAll('.toCart');
 
 allProducts.forEach((product) => {
-    let btnPlus = product.querySelector('.btnPlus');
-    let btnMinus = product.querySelector('.btnMinus');
-    let quantDisplay = product.querySelector('.productQuant');
+    let btnPlus = product.querySelector('input.btnPlus, input.btnPlus2, input.btnPlus3');
+    let btnMinus = product.querySelector('input.btnMinus, input.btnMinus2, input.btnMinus3');
+    let quantDisplay = product.querySelector('.productQuant, .productQuant2, .productQuant3');
+    let btnAdd = product.querySelector('button.addToCart, button.addToCart2, button.addToCart3');
+
     let count = 0;
 
-    btnPlus.addEventListener('click', () => {
-        count++;
-        quantDisplay.innerHTML = count;
-    });
-
-    btnMinus.addEventListener('click', () => {
-        if (count > 0) {
-            count--;
+    if (btnPlus && quantDisplay) {
+        btnPlus.addEventListener('click', () => {
+            count++;
             quantDisplay.innerHTML = count;
-        }
-    });
+        });
+    }
+
+    if (btnMinus && quantDisplay) {
+        btnMinus.addEventListener('click', () => {
+            if (count > 0) {
+                count--;
+                quantDisplay.innerHTML = count;
+            }
+        });
+    }
+
+    if (btnAdd && quantDisplay) {
+        btnAdd.addEventListener('click', () => {
+            const txtCart = document.querySelector('.txtCart');
+            txtCart.innerHTML = txtCart.innerHTML;
+        });
+    }
 });
 
-////////////////////////////////////////////////////////
-let btnAddToCart = document.querySelector('.addToCart');
 
-btnAddToCart.addEventListener('click', () => {
-
-})
 ////////////////////////////////////////////////////////
+let allProd = document.querySelectorAll('.toCart');
+let txtCart11 = document.querySelector('.txtCart'); 
+let txtCart12 = document.querySelector('.txtCart2'); 
+let txtCart13 = document.querySelector('.txtCart3'); 
+
+allProd.forEach((product) => {
+    let btnAddToCart = product.querySelector('.addToCart');
+    let btnAddToCart2 = product.querySelector('.addToCart2');
+    let btnAddToCart3 = product.querySelector('.addToCart3');
+
+    let quantDisplay_ = product.querySelector('.productQuant');
+    let quantDisplay_2 = product.querySelector('.productQuant2');
+    let quantDisplay_3 = product.querySelector('.productQuant3');
+
+    if (btnAddToCart && quantDisplay_) {
+        btnAddToCart.addEventListener('click', () => {
+            let quantidade = quantDisplay_.innerHTML;
+            txtCart11.innerHTML = 'Camisetas: ' + quantidade;
+        });
+    }
+
+    if (btnAddToCart2 && quantDisplay_2) {
+        btnAddToCart2.addEventListener('click', () => {
+            let quantidade = quantDisplay_2.innerHTML;
+            txtCart12.innerHTML = 'Regatas: ' + quantidade;
+        });
+    }
+
+    if (btnAddToCart3 && quantDisplay_3) {
+        btnAddToCart3.addEventListener('click', () => {
+            let quantidade = quantDisplay_3.innerHTML;
+            txtCart13.innerHTML = 'Calças: ' + quantidade;
+        });
+    }
+});
+
+
+////////////////////////////////////////////////////////
+let btnCart = document.querySelector('#cartMenu');
+let cartButton = document.querySelector('#menuCart');
+
+function cart(){
+    cartButton.classList.toggle('menuCart1');
+}
+function removeClassCart(){
+    cartButton.classList.remove('menuCart1');
+}
